55fd892f56668894b0383bd3e2c5958b2332e859
Robert Battaglia <r083rt164@gmail.com>
2019-08-06 12:09:22 -0400

    add 4 unit tests for get questions

--- server/__tests__/questions.test.js
+++ server/__tests__/questions.test.js
@@ -1,7 +1,20 @@
 const frisby = require('frisby');
 
 describe('GET Questions', () => {
+  it('should return 2 questions if given query param count of 2', () => frisby.get('http://localhost:3000/qa/1?count=2').then((data) => {
+    const { results } = JSON.parse(data.body);
+    const questionsLength = results.length;
+    expect(questionsLength).toBe(2);
+  }));
+  it('should return questions for product id in url', () => {
+    const num = Math.floor(Math.random() * 1000);
+    return frisby.get(`http://localhost:3000/qa/${num}`).expect('json', 'product_id', num);
+  });
   it('should return status 200', () => frisby.get('http://localhost:3000/qa/1').expect('status', 200));
+  it('should be returned in application/json MIMETYPE', () => frisby
+    .get('http://localhost:3000/qa/1')
+    .expect('header', 'Content-Type', 'application/json; charset=utf-8'));
+  it('should have CORS in header', () => frisby.get('http://localhost:3000/qa/1').expect('header', 'Access-Control-Allow-Origin', '*'));
 });
 
 describe('POST Question', () => {
