fc6f5c25be874d022df5e776a29d5901726bc634
Robert Battaglia <r083rt164@gmail.com>
2019-08-06 11:23:16 -0400

    figure out frisby post request

--- server/__tests__/answers.test.js
+++ server/__tests__/answers.test.js
@@ -4,14 +4,19 @@ describe('GET Answers', () => {
   it('should return status 200', () => frisby.get('http://localhost:3000/qa/1/answers').expect('status', 200));
 });
 
-// TODO: Figure out how to get Frisby to send json in request body
-// describe('POST Answer', () => {
-//   it('should return status 201', () => frisby
-//     .post('http://localhost:3000/qa/1/answers', { body: 'body', name: 'name', email: 'email', photos: ['url1', 'url2'] })
-//     .inspectRequest()
-//     .inspectBody()
-//     .expect('status', 201));
-// });
+describe('POST Answer', () => {
+  it('should return status 201', () => frisby
+    .fetch('http://localhost:3000/qa/1/answers', {
+      method: 'post',
+      body: JSON.stringify({
+        body: 'body',
+        name: 'name',
+        email: 'email',
+        photos: ['url1', 'url2'],
+      }),
+    })
+    .expect('status', 201));
+});
 
 describe('PUT Answer Helpfulness', () => {
   it('should return status 204', () => frisby.put('http://localhost:3000/qa/answer/1/helpful').expect('status', 204));
--- server/__tests__/questions.test.js
+++ server/__tests__/questions.test.js
@@ -4,14 +4,14 @@ describe('GET Questions', () => {
   it('should return status 200', () => frisby.get('http://localhost:3000/qa/1').expect('status', 200));
 });
 
-// TODO: Figure out how to get Frisby to send json in request body
-// describe('POST Question', () => {
-//   it('should return status 201', () => frisby
-//     .post('http://localhost:3000/qa/1', { body: 'body', name: 'name', email: 'email' })
-//     .inspectRequest()
-//     .inspectBody()
-//     .expect('status', 201));
-// });
+describe('POST Question', () => {
+  it('should return status 201', () => frisby
+    .fetch('http://localhost:3000/qa/1', {
+      method: 'post',
+      body: JSON.stringify({ body: 'Frisby Test', name: 'Frisby', email: 'email@frisby.com' }),
+    })
+    .expect('status', 201));
+});
 
 describe('PUT Question Helpfulness', () => {
   it('should return status 204', () => frisby.put('http://localhost:3000/qa/question/1/helpful').expect('status', 204));
